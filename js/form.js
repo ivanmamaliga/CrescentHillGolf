6133b495a1bce424773d3758fd3c5a7295e7feb0
ivanmamaliga@gmail.com <70662242+ivanmamaliga@users.noreply.github.com>
2022-06-02 13:55:15 -0400

    updated toggle bar

--- js/form.js
+++ js/form.js
@@ -1,18 +1,18 @@
-var inv = document.getElementById('inv');
-var svn = document.getElementById('sav');
-var int = document.getElementById('int');
+// var inv = document.getElementById('inv');
+// var svn = document.getElementById('sav');
+// var int = document.getElementById('int');
 
 
-document.getElementById('calc').addEventListener('submit', function(e) {
-    e.preventDefault(); //to prevent form submission
-    // alert('click');
+// document.getElementById('calc').addEventListener('submit', function(e) {
+//     e.preventDefault(); //to prevent form submission
+//     // alert('click');
    
-    if (inv.value.length == 0 || svn.value.length == 0 || int.value.length == 0) {
-      alert("PLease enter the values an all required cells");
-    } else {
-        document.getElementById('poff').value = (parseFloat(inv.value) * (parseFloat(int.value)/100 + 1) / parseFloat(svn.value)).toFixed(2) + " years";
-    }
-  });
+//     if (inv.value.length == 0 || svn.value.length == 0 || int.value.length == 0) {
+//       alert("PLease enter the values an all required cells");
+//     } else {
+//         document.getElementById('poff').value = (parseFloat(inv.value) * (parseFloat(int.value)/100 + 1) / parseFloat(svn.value)).toFixed(2) + " years";
+//     }
+//   });
 
 //User clicks on the Reset button and the fields a reset to null
 
